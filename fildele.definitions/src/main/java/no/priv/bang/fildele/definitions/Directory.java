4e01bfbd9cfe8963ce9936d89f530fd68dc65bf3
Steinar Bang <sb@dod.no>
2025-05-31 18:45:52 +0200

    Delete the 2019 fildele application

--- fildele.definitions/src/main/java/no/priv/bang/fildele/definitions/Directory.java
+++
@@ -1,7 +0,0 @@
-package no.priv.bang.fildele.definitions;
-
-import no.priv.bang.beans.immutable.Immutable;
-
-public class Directory extends Immutable { // NOSONAR Immutable provides a satisfactory equals() method
-
-}
